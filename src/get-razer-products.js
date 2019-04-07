893cb887dfcee589d9732548fa4fad69c51978fd
Lester Chan <lesterchan@gmail.com>
2019-04-07 15:17:23 +0800

    Razer uses SEO URL to check for stocks since product number differs from sites to sites

--- src/get-razer-products.js
+++ src/get-razer-products.js
@@ -7,45 +7,70 @@ const { fetchPageContents, writeDataFile } = require('./helper');
 const razerUrl = 'https://www.razer.com';
 const storeSlug = '/store';
 const productJsonFile = '../data/razer-products.json';
-const productLinkElement = 'a.gtm_learn_more';
-const productParentLinkElement = 'ul.text-16.collapsible-menu li.text-16 a';
+const productHeroLinkElement = 'a.categorised-content-latest';
+const productLinkElement = 'div.cta a.gtm_learn_more';
+const productParentMenuElement = 'ul.menu.level-one';
+const productParentLinkElement = 'li.level0.desktop-menu a';
 
-const fetchProductParentLinks = async (storeUrl, parentLinkElement) => {
+const fetchProductParentLinks = async (storeUrl, parentMenuElement, parentLinkElement) => {
   const pageHtml = await fetchPageContents(storeUrl);
   const $ = cheerio.load(pageHtml);
   const data = [];
-  $(parentLinkElement).each(async (i, elem) => {
-    const productParentLink = $(elem).attr('href');
-    const productParentName = $(elem).text().trim();
-    if (productParentLink === storeSlug) {
-      return;
-    }
-    data.push({ name: productParentName, link: productParentLink, products: [] });
-  });
+  $(parentMenuElement).children().first().find(parentLinkElement)
+    .each(async (i, elem) => {
+      const productParentLink = $(elem).attr('href');
+      const productParentName = $(elem).text().trim().replace(' New', '');
+      if (productParentLink === storeSlug) {
+        return;
+      }
+      if (productParentLink.includes('http')) {
+        return;
+      }
+      data.push({ name: productParentName, link: productParentLink, products: [] });
+    });
 
   return data;
 };
 
-const fetchProductLinks = async (parentLink, linkElement) => {
+const fetchProductLinks = async (parentLink, heroLinkElement, linkElement) => {
   const productPageHtml = await fetchPageContents(parentLink);
   const $productPage = cheerio.load(productPageHtml);
-  const data = [];
-  $productPage(linkElement).each((index, element) => {
-    const productLink = $productPage(element).attr('href');
-    const productName = $productPage('h2', element).text().trim();
+  const data = {};
+  $productPage(heroLinkElement).each((index, element) => {
+    const productLink = $productPage(element).attr('href').replace('./', '/').replace(razerUrl, '');
+    const productName = $productPage(element).children().children().children('h2')
+      .text()
+      .trim();
     if (productLink.startsWith('#')) {
       return;
     }
-    data.push({ name: productName, link: productLink });
+    if (productLink.startsWith('javascript')) {
+      return;
+    }
+    if (!data[productLink]) {
+      data[productLink] = productName;
+    }
+  });
+  $productPage(linkElement).each((index, element) => {
+    const productLink = $productPage(element).attr('href');
+    const productName = $productPage(element).parent().parent().find('h2')
+      .text()
+      .trim();
+    if (productLink.startsWith('#')) {
+      return;
+    }
+    if (!data[productLink]) {
+      data[productLink] = productName;
+    }
   });
 
   return data;
 };
 
 (async () => {
-  const productParentLinks = await fetchProductParentLinks(razerUrl + storeSlug, productParentLinkElement);
+  const productParentLinks = await fetchProductParentLinks(razerUrl + storeSlug, productParentMenuElement, productParentLinkElement);
   const productsPromises = productParentLinks.map(async (productParent) => {
-    const products = await fetchProductLinks(razerUrl + productParent.link, productLinkElement);
+    const products = await fetchProductLinks(razerUrl + productParent.link.replace(storeSlug, ''), productHeroLinkElement, productLinkElement);
     productParent.products = products;
     return productParent;
   });
